91dc63423a9e3e0256bb4cb82392405c6ae4bb4f
Samska <samuel.andradetp@live.com>
2022-12-23 14:13:55 -0300

    changed project structure and added junit as a runner

---
+++ src/test/java/com/examples/cucumber/LearnCucumber.java
@@ -0,0 +1,20 @@
+package com.examples.cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class LearnCucumber {
+    @Given("that I have created the file correctly")
+    public void thatIHaveCreatedTheFileCorrectly() {
+
+    }
+    @When("I run it")
+    public void iRunIt() {
+
+    }
+    @Then("the specification should finish successfully")
+    public void theSpecificationShouldFinishSuccessfully() {
+
+    }
+}

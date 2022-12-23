f8bd3e057fd1eb55ab2bb0767e7bfb95a50ab29c
Samska <samuel.andradetp@live.com>
2022-12-23 12:00:33 -0300

    updates

--- src/test/java/LearnCucumber.java
+++ src/test/java/LearnCucumber.java
@@ -5,16 +5,16 @@ import io.cucumber.java.en.Then;
 public class LearnCucumber {
     @Given("that I have created the file correctly")
     public void that_i_have_created_the_file_correctly() {
-
+        System.out.println("teste");
     }
 
     @When("I run it")
     public void i_run_it() {
-
+        System.out.println("teste");
     }
 
     @Then("the specification should finish successfully")
     public void the_specification_should_finish_successfully() {
-
+        System.out.println("teste");
     }
 }

256cbad58afc0fd5944370d1dc58d6d827f56ba2
Samska <samuel.andradetp@live.com>
2022-12-23 11:33:38 -0300

    include feature scenario and cucumber steps

---
+++ src/test/java/LearnCucumber.java
@@ -0,0 +1,20 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class LearnCucumber {
+    @Given("that I have created the file correctly")
+    public void that_i_have_created_the_file_correctly() {
+
+    }
+
+    @When("I run it")
+    public void i_run_it() {
+
+    }
+
+    @Then("the specification should finish successfully")
+    public void the_specification_should_finish_successfully() {
+
+    }
+}
---
+++ src/test/java/learn_cucumber.feature
@@ -0,0 +1,9 @@
+Feature: Learn Cucumber
+  As a student
+  I want to learn how to use Cucumber
+  So that I can automate acceptance criteria
+
+  Scenario: It must execute the specification
+    Given that I have created the file correctly
+    When I run it
+    Then the specification should finish successfully
